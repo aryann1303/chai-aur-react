8170b329c77cb857e26359fe40767f3d56e5339c
aryann1303 <aryanbharadwaj4554@gmail.com>
2025-01-12 21:47:14 +0530

    interview54

---
+++ 06currencyConverter/currency.md
@@ -0,0 +1,5 @@
+https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies.json
+
+
+
+https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies/${country}.json
---
+++ 07Try/src/components/Card.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import './Card.css'
+
+const Card = () => {
+  return (
+    <div>
+        <div className='outer'>
+            This is first div
+            <input type="text" />
+        </div>
+        <button className='btn'></button>
+        
+    </div>
+  )
+}
+
+export default Card;
